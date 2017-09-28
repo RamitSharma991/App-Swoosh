7edfe1afcd324d0ca846329b1ce344c5172da1e9
RamitSharma991 <RamitsMac@Ramits-MacBook-Pro.local>
2017-09-28 15:09:43 +0530

    Homescreen UI

---
+++ App-Swoosh/App-Swoosh/UIViews/BorderButton.swift
@@ -0,0 +1,31 @@
+//
+//  BorderButton.swift
+//  App-Swoosh
+//
+//  Created by Ramit sharma on 28/09/17.
+//  Copyright © 2017 Ramit sharma. All rights reserved.
+//
+
+import UIKit
+
+class BorderButton: UIButton {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.borderWidth = 2.0
+        layer.borderColor = UIColor.white.cgColor
+    }
+    
+    
+    
+    
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
