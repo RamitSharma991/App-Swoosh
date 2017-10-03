884f29bb48e0983d4249b366c11f330d6d31285a
RamitSharma991 <RamitsMac@Ramits-MacBook-Pro.local>
2017-10-03 15:17:35 +0530

    segues and unwind

--- App-Swoosh/App-Swoosh/ViewController.swift
+++ App-Swoosh/App-Swoosh/ViewController.swift
@@ -25,4 +25,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+   @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboard) {
+        
+        
+    }
 }
